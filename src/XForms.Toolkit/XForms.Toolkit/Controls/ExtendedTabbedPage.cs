1a45a8f801a34bceb41e2f194fb090ae1881ddb5
ThomasLebrun <lebrun_thomas@hotmail.com>
2014-06-04 21:15:38 +0200

    Added ExtendedTabbedPage control.

---
+++ src/XForms.Toolkit/XForms.Toolkit/Controls/ExtendedTabbedPage.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace XForms.Toolkit.Controls
+{
+    public delegate void CurrentPageChangingEventHandler();
+    public delegate void CurrentPageChangedEventHandler();
+
+    public class ExtendedTabbedPage : TabbedPage
+    {
+        public event CurrentPageChangingEventHandler CurrentPageChanging;
+        public event CurrentPageChangedEventHandler CurrentPageChanged;
+
+        public ExtendedTabbedPage()
+        {
+            this.PropertyChanging += this.OnPropertyChanging;
+            this.PropertyChanged += this.OnPropertyChanged;
+        }
+
+        private void OnPropertyChanging(object sender, PropertyChangingEventArgs e)
+        {
+            if (e.PropertyName == "CurrentPage")
+            {
+                this.RaiseCurrentPageChanging();
+            }
+        }
+
+        private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "CurrentPage")
+            {
+                this.RaiseCurrentPageChanged();
+            }
+        }
+
+        private void RaiseCurrentPageChanging()
+        {
+            var handler = this.CurrentPageChanging;
+            if (handler != null)
+            {
+                handler();
+            }
+        }
+
+        private void RaiseCurrentPageChanged()
+        {
+            var handler = this.CurrentPageChanged;
+            if (handler != null)
+            {
+                handler();
+            }
+        }
+    }
+}
